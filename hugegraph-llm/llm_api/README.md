90beec2026dfc17efe0b7a303160fe491b48e67a
Vichayturen <1073931273@qq.com>
2024-04-18 12:15:44 +0800

    fix some questions

--- hugegraph-llm/llm_api/README.md
+++ hugegraph-llm/llm_api/README.md
@@ -1,9 +1,4 @@
+# local LLM Api
 
 support models:
 - qwen1.5-chat
-
-# requirements
-```shell
-conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia
-pip install transformers fastapi accelerate chardet
-```
--- hugegraph-llm/llm_api/main.py
+++ hugegraph-llm/llm_api/main.py
@@ -1,6 +1,23 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
 import gc
 import json
-import os
 from contextlib import asynccontextmanager
 from enum import Enum
 from typing import Any, Literal, List, Dict
@@ -10,7 +27,6 @@ import uvicorn
 from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-# from sse_starlette import EventSourceResponse
 from transformers import AutoModelForCausalLM, AutoTokenizer, Qwen2ForCausalLM, Qwen2Tokenizer, GenerationConfig
 from typing_extensions import TypedDict
 
@@ -131,7 +147,7 @@ def create_app(chat_model: "QwenChatModel") -> "FastAPI":
 
 
 def main():
-    model_path = r"V:\language_models\Qwen1.5-0.5B-Chat"
+    model_path = "Qwen/Qwen1.5-0.5B-Chat"
     device = "cuda"
     chat_model = QwenChatModel(model_path, device)
     app = create_app(chat_model)
