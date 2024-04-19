40ea8ba2c1b8c656e3c3681ea7256964e03eed3c
Vichayturen <1073931273@qq.com>
2024-04-19 09:07:11 +0800

    1. Removed unused function. 2. Added one configurable arguments max_new_tokens to command line argumnents. 3. Fixed some code style issues.

--- hugegraph-llm/llm_api/main.py
+++ hugegraph-llm/llm_api/main.py
@@ -54,7 +54,7 @@ class Role(Enum):
 
 
 class QwenChatModel:
-    def __init__(self, model_name_or_path: str, device: str = "cuda",
+    def __init__(self, model_name_or_path: str, device: str = "cuda", max_new_tokens: int = 512,
                  generation_config: GenerationConfig = None):
         self.model: Qwen2ForCausalLM = AutoModelForCausalLM.from_pretrained(
             model_name_or_path,
@@ -63,6 +63,7 @@ class QwenChatModel:
         )
         self.tokenizer: Qwen2Tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
         self.device = torch.device(device)
+        self.max_new_tokens = max_new_tokens
         self.generation_config = generation_config
 
     @torch.inference_mode()
@@ -75,7 +76,7 @@ class QwenChatModel:
         model_inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
         generated_ids = self.model.generate(
             model_inputs.input_ids,
-            max_new_tokens=512,
+            max_new_tokens=self.max_new_tokens,
         )
         generated_ids = [
             output_ids[len(input_ids):]
@@ -99,20 +100,6 @@ def torch_gc() -> None:
 async def lifespan(app: "FastAPI"):  # collects GPU memory
     yield
     torch_gc()
-
-
-def dictify(data: "BaseModel") -> Dict[str, Any]:
-    try:  # pydantic v2
-        return data.model_dump(exclude_unset=True)
-    except AttributeError:  # pydantic v1
-        return data.dict(exclude_unset=True)
-
-
-def jsonify(data: "BaseModel") -> str:
-    try:  # pydantic v2
-        return json.dumps(data.model_dump(exclude_unset=True), ensure_ascii=False)
-    except AttributeError:  # pydantic v1
-        return data.json(exclude_unset=True, ensure_ascii=False)
 
 
 def create_app(chat_model: "QwenChatModel") -> "FastAPI":
@@ -155,13 +142,15 @@ def main():
     parser.add_argument("--model_name_or_path", type=str, required=True, help="Model name or path")
     parser.add_argument("--device", type=str, default="cpu", help="Device to use")
     parser.add_argument("--port", type=int, default=7999, help="Port of the service")
+    parser.add_argument("--max_new_tokens", type=int, default=512, help="The max number of tokens to generate")
 
     args = parser.parse_args()
 
     model_path = args.model_name_or_path
     device = args.device
     port = args.port
-    chat_model = QwenChatModel(model_path, device)
+    max_new_tokens = args.max_new_tokens
+    chat_model = QwenChatModel(model_path, device, max_new_tokens)
     app = create_app(chat_model)
     uvicorn.run(app, host="0.0.0.0", port=port, workers=1)
 
