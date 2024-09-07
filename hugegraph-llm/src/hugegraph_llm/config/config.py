1dc612cc35d23fb7c32d3c668a7c0e1ceccce4f8
Hongjun Li <returntoinnocencegeek@outlook.com>
2024-09-07 13:07:29 +0800

    feat(llm): Add an apache license header to config prompt.yaml

--- hugegraph-llm/src/hugegraph_llm/config/config.py
+++ hugegraph-llm/src/hugegraph_llm/config/config.py
@@ -225,6 +225,24 @@ Meet Sarah, a 30-year-old attorney, and her roommate, James, whom she's shared a
 [{"id":"1:Sarah","label":"person","type":"vertex","properties":{"name":"Sarah","age":30,"occupation":"attorney"}},{"id":"1:James","label":"person","type":"vertex","properties":{"name":"James","occupation":"journalist"}},{"label":"roommate","type":"edge","outV":"1:Sarah","outVLabel":"person","inV":"1:James","inVLabel":"person","properties":{"date":"2010"}}]
     """
 
+    apache_license_header = """# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""
+
     def __init__(self):
         self.ensure_yaml_file_exists()
 
@@ -244,7 +262,8 @@ Meet Sarah, a 30-year-old attorney, and her roommate, James, whom she's shared a
         indented_schema = "\n".join([f"  {line}" for line in self.rag_schema.splitlines()])
         indented_example_prompt = "\n".join([f"    {line}" for line in self.schema_example_prompt.splitlines()])
         # This can be extended to add storage fields according to the data needs to be stored
-        yaml_content = f"""
+        yaml_content = f"""{self.apache_license_header}
+
 rag_schema: |
 {indented_schema}
 
