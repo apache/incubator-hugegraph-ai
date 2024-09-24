5bc958ca1f61f996f32817dca9e59e862062074c
vichayturen <1073931273@qq.com>
2024-09-24 22:34:49 +0800

    fix: remove 'llm' objetc in json response

--- hugegraph-llm/src/hugegraph_llm/api/rag_api.py
+++ hugegraph-llm/src/hugegraph_llm/api/rag_api.py
@@ -72,6 +72,8 @@ def rag_http_api(
             )
             # TODO/FIXME: handle QianFanClient error (not dict..critical)
             # log.critical(f"## {type(result)}, {json.dumps(result)}")
+            if "llm" in result:
+                result.pop("llm")
             if isinstance(result, dict):
                 log.critical(f"##1. {type(result)}")
                 return {"graph_recall": result}
