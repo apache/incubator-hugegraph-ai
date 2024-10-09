7b00dec7a4bd1fe70f8a14da329b2d039d984e0c
Hongjun Li <returntoinnocencegeek@outlook.com>
2024-10-09 19:20:23 +0800

    chore(rag): Modify the comments to leave a general idea of the unresolved part

--- hugegraph-llm/src/hugegraph_llm/demo/rag_demo/app.py
+++ hugegraph-llm/src/hugegraph_llm/demo/rag_demo/app.py
@@ -62,27 +62,23 @@ def init_rag_ui() -> gr.Interface:
     ) as hugegraph_llm_ui:
         gr.Markdown("# HugeGraph LLM RAG Demo")
 
-        # create_configs_block()
-
+        # TODO: leave a general idea of the unresolved part
         # graph_config_input = textbox_array_graph_config
         #  = [settings.graph_ip, settings.graph_port, settings.graph_name, graph_user, settings.graph_pwd, settings.graph_space]
         
         # llm_config_input = textbox_array_llm_config
-        # 判断 settings.llm_type，但是这个值似乎也缺少一个刷新机制
         #  = if settings.llm_type == openai [settings.openai_api_key, settings.openai_api_base, settings.openai_language_model, settings.openai_max_tokens]
         #  = else if settings.llm_type == ollama [settings.ollama_host, settings.ollama_port, settings.ollama_language_model, ""]
         #  = else if settings.llm_type == qianfan_wenxin [settings.qianfan_api_key, settings.qianfan_secret_key, settings.qianfan_language_model, ""]
-        #  = else ["","","", ""] 这个是错误情况
+        #  = else ["","","", ""]
 
         # embedding_config_input = textbox_array_embedding_config
-        # 判断 settings.embedding_type，但是这个值也缺少一个刷新机制
         #  = if settings.embedding_type == openai [settings.openai_api_key, settings.openai_api_base, settings.openai_embedding_model]
         #  = else if settings.embedding_type == ollama [settings.ollama_host, settings.ollama_port, settings.ollama_embedding_model]
         #  = else if settings.embedding_type == qianfan_wenxin [settings.qianfan_api_key, settings.qianfan_secret_key, settings.qianfan_embedding_model]
         #  = else ["","",""]
 
         # reranker_config_input = textbox_array_reranker_config
-        # 判断 settings.reranker_type，但是这个值也缺少一个刷新机制
         #  = if settings.reranker_type == cohere [settings.reranker_api_key, settings.reranker_model, settings.cohere_base_url]
         #  = else if settings.reranker_type == siliconflow [settings.reranker_api_key, "BAAI/bge-reranker-v2-m3", ""]
         #  = else ["","",""]
