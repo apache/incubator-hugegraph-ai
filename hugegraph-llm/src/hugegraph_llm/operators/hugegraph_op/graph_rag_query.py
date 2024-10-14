7b02fd43767b7f333cae04ad87f09981cb0778fc
imbajin <jin@apache.org>
2024-10-14 19:14:23 +0800

    refactor(llm): enhance a string of graph query method (#89)
    * feat: Modify the method of importing graph by extracting from the entire document
    
    * fix: remove 'llm' objetc in json response
    
    * fix: remove 'llm' object in context transferring
    
    * TODO: only fetch vid rather than all vertex data
    
    * refactor(llm): enhance a string of graph query method
    
    * refactor(llm): enhance property query template
    
    * refactor: enhance graphrag format logic
    
    * refactor: enhance vector extract/fuzz logic & add dis_threshold for it
    
    * improve & log
    
    * filter single char & remove it from prompt
    
    ---------
    
    Co-authored-by: Vichayturen <1073931273@qq.com>

--- hugegraph-llm/src/hugegraph_llm/operators/hugegraph_op/graph_rag_query.py
+++ hugegraph-llm/src/hugegraph_llm/operators/hugegraph_op/graph_rag_query.py
@@ -14,9 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-
-import re
 from typing import Any, Dict, Optional, List, Set, Tuple
 
 from hugegraph_llm.config import settings
@@ -26,12 +23,13 @@ from pyhugegraph.client import PyHugeClient
 VERTEX_QUERY_TPL = "g.V({keywords}).as('subj').toList()"
 
 # TODO: we could use a simpler query (like kneighbor-api to get the edges)
-# TODO: use dedup() to filter duplicate paths
-ID_QUERY_NEIGHBOR_TPL = """
-g.V({keywords}).as('subj')
+# TODO: test with profile()/explain() to speed up the query
+VID_QUERY_NEIGHBOR_TPL = """
+g.V({keywords})
 .repeat(
-   bothE({edge_labels}).as('rel').otherV().as('obj')
-).times({max_deep})
+   bothE({edge_labels}).limit({edge_limit}).otherV().dedup()
+).times({max_deep}).emit()
+.simplePath()
 .path()
 .by(project('label', 'id', 'props')
    .by(label())
@@ -49,10 +47,11 @@ g.V({keywords}).as('subj')
 """
 
 PROPERTY_QUERY_NEIGHBOR_TPL = """
-g.V().has('{prop}', within({keywords})).as('subj')
+g.V().has('{prop}', within({keywords}))
 .repeat(
-   bothE({edge_labels}).as('rel').otherV().as('obj')
-).times({max_deep})
+   bothE({edge_labels}).limit({edge_limit}).otherV().dedup()
+).times({max_deep}).emit()
+.simplePath()
 .path()
 .by(project('label', 'props')
    .by(label())
@@ -71,7 +70,7 @@ g.V().has('{prop}', within({keywords})).as('subj')
 
 class GraphRAGQuery:
 
-    def __init__(self, max_deep: int = 2, max_items: int = 30, prop_to_match: Optional[str] = None):
+    def __init__(self, max_deep: int = 2, max_items: int = 20, prop_to_match: Optional[str] = None):
         self._client = PyHugeClient(
             settings.graph_ip,
             settings.graph_port,
@@ -100,9 +99,8 @@ class GraphRAGQuery:
                 self._client = PyHugeClient(ip, port, graph, user, pwd, gs)
         assert self._client is not None, "No valid graph to search."
 
-        keywords = context.get("keywords")
-        match_vids = context.get("match_vids")
-
+        # 2. Extract params from context
+        matched_vids = context.get("match_vids")
         if isinstance(context.get("max_deep"), int):
             self._max_deep = context["max_deep"]
         if isinstance(context.get("max_items"), int):
@@ -110,30 +108,34 @@ class GraphRAGQuery:
         if isinstance(context.get("prop_to_match"), str):
             self._prop_to_match = context["prop_to_match"]
 
+        # 3. Extract edge_labels from graph schema
         _, edge_labels = self._extract_labels_from_schema()
         edge_labels_str = ",".join("'" + label + "'" for label in edge_labels)
+        # TODO: enhance the limit logic later
+        edge_limit_amount = len(edge_labels) * 10
 
         use_id_to_match = self._prop_to_match is None
         if use_id_to_match:
-            if not match_vids:
+            if not matched_vids:
                 return context
 
-            gremlin_query = VERTEX_QUERY_TPL.format(keywords=match_vids)
-            result: List[Any] = self._client.gremlin().exec(gremlin=gremlin_query)["data"]
-            log.debug("Vids query: %s", gremlin_query)
+            gremlin_query = VERTEX_QUERY_TPL.format(keywords=matched_vids)
+            vertexes = self._client.gremlin().exec(gremlin=gremlin_query)["data"]
+            log.debug("Vids gremlin query: %s", gremlin_query)
 
-            vertex_knowledge = self._format_graph_from_vertex(query_result=result)
-            gremlin_query = ID_QUERY_NEIGHBOR_TPL.format(
-                keywords=match_vids,
+            vertex_knowledge = self._format_graph_from_vertex(query_result=vertexes)
+            gremlin_query = VID_QUERY_NEIGHBOR_TPL.format(
+                keywords=matched_vids,
                 max_deep=self._max_deep,
-                max_items=self._max_items,
                 edge_labels=edge_labels_str,
+                edge_limit=edge_limit_amount,
+                max_items=self._max_items,
             )
-            log.debug("Kneighbor query: %s", gremlin_query)
+            log.debug("Kneighbor gremlin query: %s", gremlin_query)
+            paths = self._client.gremlin().exec(gremlin=gremlin_query)["data"]
 
-            result: List[Any] = self._client.gremlin().exec(gremlin=gremlin_query)["data"]
-            graph_chain_knowledge, vertex_degree_list, knowledge_with_degree = self._format_graph_from_query_result(
-                query_result=result
+            graph_chain_knowledge, vertex_degree_list, knowledge_with_degree = self._format_graph_query_result(
+                query_paths=paths
             )
             graph_chain_knowledge.update(vertex_knowledge)
             if vertex_degree_list:
@@ -142,29 +144,30 @@ class GraphRAGQuery:
                 vertex_degree_list.append(vertex_knowledge)
         else:
             # WARN: When will the query enter here?
+            keywords = context.get("keywords")
             assert keywords, "No related property(keywords) for graph query."
             keywords_str = ",".join("'" + kw + "'" for kw in keywords)
             gremlin_query = PROPERTY_QUERY_NEIGHBOR_TPL.format(
                 prop=self._prop_to_match,
                 keywords=keywords_str,
+                edge_labels=edge_labels_str,
+                edge_limit=edge_limit_amount,
                 max_deep=self._max_deep,
                 max_items=self._max_items,
-                edge_labels=edge_labels_str,
             )
             log.warning("Unable to find vid, downgraded to property query, please confirm if it meets expectation.")
 
-            result: List[Any] = self._client.gremlin().exec(gremlin=gremlin_query)["data"]
-            graph_chain_knowledge, vertex_degree_list, knowledge_with_degree = self._format_graph_from_query_result(
-                query_result=result
+            paths: List[Any] = self._client.gremlin().exec(gremlin=gremlin_query)["data"]
+            graph_chain_knowledge, vertex_degree_list, knowledge_with_degree = self._format_graph_query_result(
+                query_paths=paths
             )
 
         context["graph_result"] = list(graph_chain_knowledge)
         context["vertex_degree_list"] = [list(vertex_degree) for vertex_degree in vertex_degree_list]
         context["knowledge_with_degree"] = knowledge_with_degree
         context["graph_context_head"] = (
-            f"The following are knowledge sequence in max depth {self._max_deep} "
-            f"in the form of directed graph like:\n"
-            "`subject -[predicate]-> object <-[predicate_next_hop]- object_next_hop ...`"
+            f"The following are graph knowledge in {self._max_deep} depth, e.g:\n"
+            "`vertexA --[links]--> vertexB <--[links]-- vertexC ...`"
             "extracted based on key entities as subject:\n"
         )
 
@@ -172,7 +175,7 @@ class GraphRAGQuery:
         verbose = context.get("verbose") or False
         if verbose:
             print("\033[93mKnowledge from Graph:")
-            print("\n".join(chain for chain in context["graph_result"]) + "\033[0m")
+            print("\n".join(context["graph_result"]) + "\033[0m")
 
         return context
 
@@ -184,65 +187,93 @@ class GraphRAGQuery:
             knowledge.add(node_str)
         return knowledge
 
-    def _format_graph_from_query_result(
-        self, query_result: List[Any]
-    ) -> Tuple[Set[str], List[Set[str]], Dict[str, List[str]]]:
+    def _format_graph_query_result(self, query_paths) -> Tuple[Set[str], List[Set[str]], Dict[str, List[str]]]:
         use_id_to_match = self._prop_to_match is None
-        knowledge = set()
-        knowledge_with_degree = {}
+        subgraph = set()
+        subgraph_with_degree = {}
         vertex_degree_list: List[Set[str]] = []
-        for line in query_result:
-            flat_rel = ""
-            raw_flat_rel = line["objects"]
-            assert len(raw_flat_rel) % 2 == 1
-            node_cache = set()
-            prior_edge_str_len = 0
-            depth = 0
-            nodes_with_degree = []
-            for i, item in enumerate(raw_flat_rel):
-                if i % 2 == 0:
-                    matched_str = item["id"] if use_id_to_match else item["props"][self._prop_to_match]
-                    if matched_str in node_cache:
-                        flat_rel = flat_rel[:-prior_edge_str_len]
-                        break
-                    node_cache.add(matched_str)
-                    props_str = ", ".join(f"{k}: {v}" for k, v in item["props"].items())
-                    node_str = f"{item['id']}{{{props_str}}}"
-                    flat_rel += node_str
-                    nodes_with_degree.append(node_str)
-                    if flat_rel in knowledge:
-                        knowledge.remove(flat_rel)
-                        knowledge_with_degree.pop(flat_rel)
-                    if depth >= len(vertex_degree_list):
-                        vertex_degree_list.append(set())
-                    vertex_degree_list[depth].add(node_str)
-                    depth += 1
-                else:
-                    props_str = ", ".join(f"{k}: {v}" for k, v in item["props"].items())
-                    props_str = f"{{{props_str}}}" if len(props_str) > 0 else ""
-                    prev_matched_str = (
-                        raw_flat_rel[i - 1]["id"]
-                        if use_id_to_match
-                        else raw_flat_rel[i - 1]["props"][self._prop_to_match]
-                    )
-                    if item["outV"] == prev_matched_str:
-                        edge_str = f" -[{item['label']}{props_str}]-> "
-                    else:
-                        edge_str = f" <-[{item['label']}{props_str}]- "
-                    flat_rel += edge_str
-                    prior_edge_str_len = len(edge_str)
-            knowledge.add(flat_rel)
-            knowledge_with_degree[flat_rel] = nodes_with_degree
-        return knowledge, vertex_degree_list, knowledge_with_degree
+
+        for path in query_paths:
+            # 1. Process each path
+            flat_rel, nodes_with_degree = self._process_path(path, use_id_to_match)
+            subgraph.add(flat_rel)
+            subgraph_with_degree[flat_rel] = nodes_with_degree
+            # 2. Update vertex degree list
+            self._update_vertex_degree_list(vertex_degree_list, nodes_with_degree)
+
+        return subgraph, vertex_degree_list, subgraph_with_degree
+
+    def _process_path(self, path: Any, use_id_to_match: bool) -> Tuple[str, List[str]]:
+        flat_rel = ""
+        raw_flat_rel = path["objects"]
+        assert len(raw_flat_rel) % 2 == 1, "The length of raw_flat_rel should be odd."
+
+        node_cache = set()
+        prior_edge_str_len = 0
+        depth = 0
+        nodes_with_degree = []
+
+        for i, item in enumerate(raw_flat_rel):
+            if i % 2 == 0:
+                # Process each vertex
+                flat_rel, prior_edge_str_len, depth = self._process_vertex(
+                    item, flat_rel, node_cache, prior_edge_str_len, depth, nodes_with_degree, use_id_to_match
+                )
+            else:
+                # Process each edge
+                flat_rel, prior_edge_str_len = self._process_edge(
+                    item, flat_rel, prior_edge_str_len, raw_flat_rel, i,use_id_to_match
+                )
+
+        return flat_rel, nodes_with_degree
+
+    def _process_vertex(self, item: Any, flat_rel: str, node_cache: Set[str],
+                        prior_edge_str_len: int, depth: int, nodes_with_degree: List[str],
+                        use_id_to_match: bool) -> Tuple[str, int, int]:
+        matched_str = item["id"] if use_id_to_match else item["props"][self._prop_to_match]
+        if matched_str in node_cache:
+            flat_rel = flat_rel[:-prior_edge_str_len]
+            return flat_rel, prior_edge_str_len, depth
+
+        node_cache.add(matched_str)
+        props_str = ", ".join(f"{k}: {v}" for k, v in item["props"].items())
+        node_str = f"{item['id']}{{{props_str}}}"
+        flat_rel += node_str
+        nodes_with_degree.append(node_str)
+        depth += 1
+
+        return flat_rel, prior_edge_str_len, depth
+
+    def _process_edge(self, item: Any, flat_rel: str, prior_edge_str_len: int,
+                      raw_flat_rel: List[Any], i: int, use_id_to_match: bool) -> Tuple[str, int]:
+        props_str = ", ".join(f"{k}: {v}" for k, v in item["props"].items())
+        props_str = f"{{{props_str}}}" if len(props_str) > 0 else ""
+        prev_matched_str = raw_flat_rel[i - 1]["id"] if use_id_to_match else raw_flat_rel[i - 1]["props"][self._prop_to_match]
+
+        if item["outV"] == prev_matched_str:
+            edge_str = f" --[{item['label']}{props_str}]--> "
+        else:
+            edge_str = f" <--[{item['label']}{props_str}]-- "
+
+        flat_rel += edge_str
+        prior_edge_str_len = len(edge_str)
+        return flat_rel, prior_edge_str_len
+
+    def _update_vertex_degree_list(self, vertex_degree_list: List[Set[str]], nodes_with_degree: List[str]) -> None:
+        for depth, node_str in enumerate(nodes_with_degree):
+            if depth >= len(vertex_degree_list):
+                vertex_degree_list.append(set())
+            vertex_degree_list[depth].add(node_str)
 
     def _extract_labels_from_schema(self) -> Tuple[List[str], List[str]]:
         schema = self._get_graph_schema()
-        node_props_str, edge_props_str = schema.split("\n")[:2]
-        node_props_str = node_props_str[len("Node properties: "):].strip("[").strip("]")
+        vertex_props_str, edge_props_str = schema.split("\n")[:2]
+        # TODO: rename to vertex (also need update in the schema)
+        vertex_props_str = vertex_props_str[len("Vertex properties: "):].strip("[").strip("]")
         edge_props_str = edge_props_str[len("Edge properties: "):].strip("[").strip("]")
-        node_labels = self._extract_label_names(node_props_str)
+        vertex_labels = self._extract_label_names(vertex_props_str)
         edge_labels = self._extract_label_names(edge_props_str)
-        return node_labels, edge_labels
+        return vertex_labels, edge_labels
 
     @staticmethod
     def _extract_label_names(source: str, head: str = "name: ", tail: str = ", ") -> List[str]:
@@ -254,19 +285,6 @@ class GraphRAGQuery:
                 result.append(label)
         return result
 
-    def _get_graph_id_format(self) -> str:
-        sample = self._client.gremlin().exec("g.V().limit(1)")["data"]
-        if len(sample) == 0:
-            return "EMPTY"
-        sample_id = sample[0]["id"]
-        if isinstance(sample_id, int):
-            return "INT"
-        if isinstance(sample_id, str):
-            if re.match(r"^\d+:.*", sample_id):
-                return "INT:STRING"
-            return "STRING"
-        return "UNKNOWN"
-
     def _get_graph_schema(self, refresh: bool = False) -> str:
         if self._schema and not refresh:
             return self._schema
@@ -277,8 +295,9 @@ class GraphRAGQuery:
         relationships = schema.getRelations()
 
         self._schema = (
-            f"Node properties: {vertex_schema}\n"
+            f"Vertex properties: {vertex_schema}\n"
             f"Edge properties: {edge_schema}\n"
             f"Relationships: {relationships}\n"
         )
+        log.debug("Link(Relation): %s", relationships)
         return self._schema
