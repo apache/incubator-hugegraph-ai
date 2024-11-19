7f4a488f325d3ab2691957d0b8fccc9cc9458c86
chenzihong <522023320011@smail.nju.edu.cn>
2024-11-19 22:54:33 +0800

    fix: add isolate node to graph chain

--- hugegraph-llm/src/hugegraph_llm/operators/hugegraph_op/graph_rag_query.py
+++ hugegraph-llm/src/hugegraph_llm/operators/hugegraph_op/graph_rag_query.py
@@ -142,6 +142,8 @@ class GraphRAGQuery:
             graph_chain_knowledge, vertex_degree_list, knowledge_with_degree = self._format_graph_query_result(
                 query_paths=paths
             )
+            if not graph_chain_knowledge:
+                graph_chain_knowledge.update(vertex_knowledge)
             if vertex_degree_list:
                 vertex_degree_list[0].update(vertex_knowledge)
             else:
