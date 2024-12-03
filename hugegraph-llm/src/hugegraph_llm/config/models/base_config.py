7371ad1993f1dddb946b9fd3dedd3949e8224c63
chenzihong <522023320011@smail.nju.edu.cn>
2024-12-03 20:59:58 +0800

    feat: add base config model

---
+++ hugegraph-llm/src/hugegraph_llm/config/models/base_config.py
@@ -0,0 +1,62 @@
+import os
+
+from dotenv import dotenv_values, set_key
+from pydantic_settings import BaseSettings
+from hugegraph_llm.utils.log import log
+
+dir_name = os.path.dirname
+package_path = dir_name(dir_name(dir_name(dir_name(dir_name(os.path.abspath(__file__))))))
+env_path = os.path.join(package_path, ".env")
+
+
+
+class BaseConfig(BaseSettings):
+    class Config:
+        env_file = env_path
+        case_sensitive = False
+        extra = 'ignore' # ignore extra fields to avoid ValidationError
+        env_ignore_empty = True
+
+    def generate_env(self):
+        if os.path.exists(env_path):
+            log.info("%s already exists, do you want to override with the default configuration? (y/n)", env_path)
+            update = input()
+            if update.lower() != "y":
+                return
+            self.update_env()
+        else:
+            config_dict = self.model_dump()
+            config_dict = {k.upper(): v for k, v in config_dict.items()}
+            with open(env_path, "w", encoding="utf-8") as f:
+                for k, v in config_dict.items():
+                    if v is None:
+                        f.write(f"{k}=\n")
+                    else:
+                        f.write(f"{k}={v}\n")
+            log.info("Generate %s successfully!", env_path)
+
+    def update_env(self):
+        config_dict = self.model_dump()
+        config_dict = {k.upper(): v for k, v in config_dict.items()}
+        env_config = dotenv_values(f"{env_path}")
+        for k, v in config_dict.items():
+            if k in env_config and env_config[k] == v:
+                continue
+            log.info("Update %s: %s=%s", env_path, k, v)
+            set_key(env_path, k, v, quote_mode="never")
+
+    def check_env(self):
+        config_dict = self.model_dump()
+        config_dict = {k.upper(): v for k, v in config_dict.items()}
+        env_config = dotenv_values(f"{env_path}")
+        for k, v in config_dict.items():
+            if k in env_config:
+                continue
+            log.info("Update %s: %s=%s", env_path, k, v)
+            set_key(env_path, k, v, quote_mode="never")
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if not os.path.exists(env_path):
+            self.generate_env()
+        log.info("Loading %s successfully!", env_path)
