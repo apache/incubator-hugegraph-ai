226c66160d06644dd54078272fc02911d29197e5
root <root@DESKTOP-CC3L639.localdomain>
2024-12-06 16:46:17 +0800

    v1

--- hugegraph-ml/src/hugegraph_ml/models/agnn.py
+++ hugegraph-ml/src/hugegraph_ml/models/agnn.py
@@ -31,6 +31,7 @@ from dgl.nn.pytorch.conv import AGNNConv
 from torch import nn
 import torch.nn.functional as F
 
+print("testtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt")
 
 class AGNN(nn.Module):
     def __init__(self, num_layers, in_dim, hid_dim, out_dim, dropout):
