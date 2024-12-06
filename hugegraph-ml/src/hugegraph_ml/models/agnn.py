72a87649afb1b0c7d4d575d4e966353f5ca8f491
root <root@DESKTOP-CC3L639.localdomain>
2024-12-06 17:16:30 +0800

    pylint 2

--- hugegraph-ml/src/hugegraph_ml/models/agnn.py
+++ hugegraph-ml/src/hugegraph_ml/models/agnn.py
@@ -31,8 +31,6 @@ from dgl.nn.pytorch.conv import AGNNConv
 from torch import nn
 import torch.nn.functional as F
 
-print("------------------------------------------------------------------------------asdasd-----------------------------------------------------------")
-
 class AGNN(nn.Module):
     def __init__(self, num_layers, in_dim, hid_dim, out_dim, dropout):
         super().__init__()
