d494b810799ae2f4d41ca5208b6087e04f5e340c
root <root@DESKTOP-CC3L639.localdomain>
2024-12-07 13:43:24 +0800

    requirements

--- hugegraph-ml/src/hugegraph_ml/tasks/node_classify_with_sample.py
+++ hugegraph-ml/src/hugegraph_ml/tasks/node_classify_with_sample.py
@@ -34,11 +34,7 @@ class NodeClassifyWithSample:
         self.graph = graph
         self._model = model
         self.gpu = -1
-        self._device = (
-            f"cuda:{self.gpu}"
-            if self.gpu != -1 and torch.cuda.is_available()
-            else "cpu"
-        )
+        self._device = ("cpu")
         self._early_stopping = None
         self._is_trained = False
         self.num_partitions = 100
@@ -56,7 +52,7 @@ class NodeClassifyWithSample:
             shuffle=True,
             drop_last=False,
             num_workers=0,
-            use_uva=True,
+            use_uva=False,
         )
         self._check_graph()
 
