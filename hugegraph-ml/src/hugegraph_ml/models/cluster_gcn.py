6589ec60b2702c61087c9152070b22242940b0c6
SoJ <102796027+MrJs133@users.noreply.github.com>
2024-12-09 17:15:25 +0800

    Update cluster_gcn.py
    change "pylint: disable=E1101" to `method` scope

--- hugegraph-ml/src/hugegraph_ml/models/cluster_gcn.py
+++ hugegraph-ml/src/hugegraph_ml/models/cluster_gcn.py
@@ -32,12 +32,13 @@ import torch.nn.functional as F
 import dgl.nn as dglnn
 
 class SAGE(nn.Module):
+    # pylint: disable=E1101
     def __init__(self, in_feats, n_hidden, n_classes):
         super().__init__()
         self.layers = nn.ModuleList()
-        self.layers.append(dglnn.SAGEConv(in_feats, n_hidden, "mean")) # pylint: disable=E1101
-        self.layers.append(dglnn.SAGEConv(n_hidden, n_hidden, "mean")) # pylint: disable=E1101
-        self.layers.append(dglnn.SAGEConv(n_hidden, n_classes, "mean")) # pylint: disable=E1101
+        self.layers.append(dglnn.SAGEConv(in_feats, n_hidden, "mean"))
+        self.layers.append(dglnn.SAGEConv(n_hidden, n_hidden, "mean"))
+        self.layers.append(dglnn.SAGEConv(n_hidden, n_classes, "mean"))
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, sg, x):
