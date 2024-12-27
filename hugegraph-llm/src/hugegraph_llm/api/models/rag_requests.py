39e8d486b31f6a2dd99f8a78dbd6402e91e7ce18
HaoJin Yang <1454yhj@gmail.com>
2024-12-27 16:53:37 +0800

    refactor(llm): remove enable_gql logic in api & rag block (#148)
    * feat(llm): support choose num of examples in rag
    
    ---------
    
    Co-authored-by: imbajin <jin@apache.org>

--- hugegraph-llm/src/hugegraph_llm/api/models/rag_requests.py
+++ hugegraph-llm/src/hugegraph_llm/api/models/rag_requests.py
@@ -32,7 +32,6 @@ class RAGRequest(BaseModel):
     graph_ratio: float = Query(0.5, description="The ratio of GraphRAG ans & vector ans")
     rerank_method: Literal["bleu", "reranker"] = Query("bleu", description="Method to rerank the results.")
     near_neighbor_first: bool = Query(False, description="Prioritize near neighbors in the search results.")
-    with_gremlin_tmpl: bool = Query(True, description="Use example template in text2gremlin")
     custom_priority_info: str = Query("", description="Custom information to prioritize certain results.")
     answer_prompt: Optional[str] = Query(prompt.answer_prompt, description="Prompt to guide the answer generation.")
     keywords_extract_prompt: Optional[str] = Query(
@@ -49,8 +48,9 @@ class RAGRequest(BaseModel):
 # TODO: import the default value of prompt.* dynamically
 class GraphRAGRequest(BaseModel):
     query: str = Query("", description="Query you want to ask")
-    gremlin_tmpl_num: int = Query(1, description="Number of Gremlin templates to use.")
-    with_gremlin_tmpl: bool = Query(True, description="Use example template in text2gremlin")
+    gremlin_tmpl_num: int = Query(
+        1, description="Number of Gremlin templates to use. If num <=0 means template is not provided"
+    )
     rerank_method: Literal["bleu", "reranker"] = Query("bleu", description="Method to rerank the results.")
     near_neighbor_first: bool = Query(False, description="Prioritize near neighbors in the search results.")
     custom_priority_info: str = Query("", description="Custom information to prioritize certain results.")
