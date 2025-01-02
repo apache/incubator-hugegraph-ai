3ad572fb79927326b03d4c78d6e51746f983d8d0
SoJGooo <102796027+MrJs133@users.noreply.github.com>
2025-01-02 17:06:26 +0800

    feat(llm): modify the summary info and enhance the request logic (#147)
    1. **Modify the return messages of `Get Vector Index Info`** . Display the number of elements in **"chunk vector"** and **"graph vid vector"** separately.
    `{
      "embed_dim": 1024,
      "vector_info": {
        "chunk_vector_num": 8,
        "graph_vid_vector_num": 483,
        "graph_properties_vector_num": 8
      }
    }`
    
    2. **Modify the return messages of `Get Graph Index Info`** . Display the number of vertices and edges. Display the first 100 edges.
    `{
      "num_vertices": [
        483
      ],
      "num_edges": [
        692
      ],
      "vertices": [
        "12:James",
        "12:Sarah"
        ....],
      "edges": [
        "S9:徐起>12>12>>S10:冀RM78Y7!小型轿车!徐起",
        "S9:刘雪峰>11>11>>S11:未与前车保持足以采取紧急制动措施的安全距离",
        ....],
          "vid_index": {
        "embed_dim": 1024,
        "num_vectors": 483,
        "num_vids": 483
      }
    }
      `
    
    3. **When a user simultaneously employs text and files, a `log.error` pops up.**
    
    4. add doc_input_text into config_prompt.py to **impl text in Doc(s) persistence**
    
    ---------
    
    Co-authored-by: imbajin <jin@apache.org>

--- hugegraph-llm/src/hugegraph_llm/demo/rag_demo/vector_graph_block.py
+++ hugegraph-llm/src/hugegraph_llm/demo/rag_demo/vector_graph_block.py
@@ -17,11 +17,9 @@
 
 # pylint: disable=E1101
 
-import os
-
 import gradio as gr
 
-from hugegraph_llm.config import resource_path, prompt
+from hugegraph_llm.config import prompt
 from hugegraph_llm.utils.graph_index_utils import (
     get_graph_index_info,
     clean_all_graph_index,
@@ -31,10 +29,10 @@ from hugegraph_llm.utils.graph_index_utils import (
 )
 from hugegraph_llm.utils.vector_index_utils import clean_vector_index, build_vector_index, get_vector_index_info
 
-
-def store_prompt(schema, example_prompt):
-    # update env variables: schema and example_prompt
-    if prompt.graph_schema != schema or prompt.extract_graph_prompt != example_prompt:
+def store_prompt(doc, schema, example_prompt):
+    # update env variables: doc, schema and example_prompt
+    if prompt.doc_input_text != doc or prompt.graph_schema != schema or prompt.extract_graph_prompt != example_prompt:
+        prompt.doc_input_text = doc
         prompt.graph_schema = schema
         prompt.extract_graph_prompt = example_prompt
         prompt.update_yaml_file()
@@ -53,7 +51,7 @@ def create_vector_graph_block():
   to modify it)
   - Specify the name of the HugeGraph graph instance, it will automatically get the schema from it (like 
   **"hugegraph"**)
-- Graph extract head: The user-defined prompt of graph extracting
+- Graph Extract Prompt Header: The user-defined prompt of graph extracting
 - If already exist the graph data, you should click "**Rebuild vid Index**" to update the index
 """
     )
@@ -61,16 +59,21 @@ def create_vector_graph_block():
     with gr.Row():
         with gr.Column():
             with gr.Tab("text") as tab_upload_text:
-                input_text = gr.Textbox(value="", label="Doc(s)", lines=20, show_copy_button=True)
+                input_text = gr.Textbox(
+                    value=prompt.doc_input_text,
+                    label="Doc(s)",
+                    lines=20,
+                    show_copy_button=True
+                )
             with gr.Tab("file") as tab_upload_file:
                 input_file = gr.File(
-                    value=[os.path.join(resource_path, "demo", "test.txt")],
+                    value=None,
                     label="Docs (multi-files can be selected together)",
                     file_count="multiple",
                 )
         input_schema = gr.Textbox(value=prompt.graph_schema, label="Schema", lines=15, show_copy_button=True)
         info_extract_template = gr.Textbox(
-            value=prompt.extract_graph_prompt, label="Graph extract head", lines=15, show_copy_button=True
+            value=prompt.extract_graph_prompt, label="Graph Extract Prompt Header", lines=15, show_copy_button=True
         )
         out = gr.Code(label="Output", language="json", elem_classes="code-container-edit")
 
@@ -91,37 +94,37 @@ def create_vector_graph_block():
 
     vector_index_btn0.click(get_vector_index_info, outputs=out).then(
         store_prompt,
-        inputs=[input_schema, info_extract_template],
+        inputs=[input_text, input_schema, info_extract_template],
     )
     vector_index_btn1.click(clean_vector_index).then(
         store_prompt,
-        inputs=[input_schema, info_extract_template],
+        inputs=[input_text, input_schema, info_extract_template],
     )
     vector_import_bt.click(build_vector_index, inputs=[input_file, input_text], outputs=out).then(
         store_prompt,
-        inputs=[input_schema, info_extract_template],
+        inputs=[input_text, input_schema, info_extract_template],
     )
     graph_index_btn0.click(get_graph_index_info, outputs=out).then(
         store_prompt,
-        inputs=[input_schema, info_extract_template],
+        inputs=[input_text, input_schema, info_extract_template],
     )
     graph_index_btn1.click(clean_all_graph_index).then(
         store_prompt,
-        inputs=[input_schema, info_extract_template],
+        inputs=[input_text, input_schema, info_extract_template],
     )
     graph_index_rebuild_bt.click(update_vid_embedding, outputs=out).then(
         store_prompt,
-        inputs=[input_schema, info_extract_template],
+        inputs=[input_text, input_schema, info_extract_template],
     )
 
     # origin_out = gr.Textbox(visible=False)
     graph_extract_bt.click(
         extract_graph, inputs=[input_file, input_text, input_schema, info_extract_template], outputs=[out]
-    ).then(store_prompt, inputs=[input_schema, info_extract_template], )
+    ).then(store_prompt, inputs=[input_text, input_schema, info_extract_template], )
 
     graph_loading_bt.click(import_graph_data, inputs=[out, input_schema], outputs=[out]).then(update_vid_embedding).then(
         store_prompt,
-        inputs=[input_schema, info_extract_template],
+        inputs=[input_text, input_schema, info_extract_template],
     )
 
     def on_tab_select(input_f, input_t, evt: gr.SelectData):
@@ -135,4 +138,4 @@ def create_vector_graph_block():
     tab_upload_file.select(fn=on_tab_select, inputs=[input_file, input_text], outputs=[input_file, input_text])
     tab_upload_text.select(fn=on_tab_select, inputs=[input_file, input_text], outputs=[input_file, input_text])
 
-    return input_schema, info_extract_template
+    return input_text, input_schema, info_extract_template
