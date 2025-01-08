b9b0d08da53327d361fd585fcf76f2ed4c5c6a72
chenzihong <522023320011@smail.nju.edu.cn>
2025-01-08 16:10:59 +0800

    fix(llm): change api_key default to ''

--- hugegraph-llm/src/hugegraph_llm/models/llms/openai.py
+++ hugegraph-llm/src/hugegraph_llm/models/llms/openai.py
@@ -42,6 +42,7 @@ class OpenAIClient(BaseLLM):
         max_tokens: int = 4096,
         temperature: float = 0.0,
     ) -> None:
+        api_key = api_key or ''
         self.client = OpenAI(api_key=api_key, base_url=api_base)
         self.aclient = AsyncOpenAI(api_key=api_key, base_url=api_base)
         self.model = model_name
