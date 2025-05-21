904b085b9e797dc8eb747f17cbd0e896476e6a03
Linyu <94553312+weijinglin@users.noreply.github.com>
2025-05-21 20:12:50 +0800

    Update build_semantic_index.py

--- hugegraph-llm/src/hugegraph_llm/operators/index_op/build_semantic_index.py
+++ hugegraph-llm/src/hugegraph_llm/operators/index_op/build_semantic_index.py
@@ -20,6 +20,8 @@ import asyncio
 import os
 from typing import Any, Dict
 
+from tqdm import tqdm
+
 from hugegraph_llm.config import resource_path, huge_settings
 from hugegraph_llm.indices.vector_index import VectorIndex
 from hugegraph_llm.models.embeddings.base import BaseEmbedding
