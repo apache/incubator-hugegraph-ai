182ecbacc9f34d52e93abfbbc7c4d33af74a4d7c
jinsong04 <jinsong04@MacBook-Pro-2.local>
2025-05-23 22:05:09 +0800

    using get_texts_embeddings instead of get_text_embeddings

--- hugegraph-llm/src/hugegraph_llm/operators/index_op/semantic_id_query.py
+++ hugegraph-llm/src/hugegraph_llm/operators/index_op/semantic_id_query.py
@@ -78,8 +78,8 @@ class SemanticIdQuery:
     def _fuzzy_match_vids(self, keywords: List[str]) -> List[str]:
         fuzzy_match_result = []
         for keyword in keywords:
-            keyword_vector = self.embedding.get_text_embedding(keyword)
-            results = self.vector_index.search(keyword_vector, top_k=self.topk_per_keyword,
+            keyword_vector = self.embedding.get_texts_embeddings([keyword])
+            results = self.vector_index.search(keyword_vector[0], top_k=self.topk_per_keyword,
                                                dis_threshold=float(self.vector_dis_threshold))
             if results:
                 fuzzy_match_result.extend(results[:self.topk_per_keyword])
